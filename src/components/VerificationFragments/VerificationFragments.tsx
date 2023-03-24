69e11e8e0ca0f4c0e93dbec0d2da9213307af71d
Sim Boon Long <simboonlong@gmail.com>
2023-03-24 13:00:49 +0800

    feat: ui viewer (#11)

--- src/components/VerificationFragments/VerificationFragments.tsx
+++ src/components/VerificationFragments/VerificationFragments.tsx
@@ -1,6 +1,12 @@
 import { FunctionComponent } from "react";
 import { VerificationFragment } from "@govtechsg/oa-verify";
+import { TickIcon } from "../../assets/icons";
 
+enum Status {
+  DOCUMENT_INTEGRITY = "Document has not been tampered with",
+  DOCUMENT_STATUS = "Document has been issued",
+  ISSUER_IDENTITY = "Document has been identified",
+}
 interface VerificationFragmentsProps {
   fragments: VerificationFragment[];
 }
@@ -13,11 +19,13 @@ export const VerificationFragments: FunctionComponent<
   );
 
   return (
-    <ul className="mb-4 border bg-gray-50 p-4">
+    <ul className="flex flex-wrap items-center">
       {validFragments.map((fragment) => (
-        <li className="mb-2" key={fragment.name}>
-          <p className="font-bold">{fragment.type}</p>
-          {fragment.name}: {fragment.status}
+        <li className="mb-2 lg:mb-0 mr-4" key={fragment.name}>
+          <div className="flex">
+            <TickIcon className="w-[20px] mr-2" />
+            <p>{Status[fragment.type]}</p>
+          </div>
         </li>
       ))}
     </ul>
