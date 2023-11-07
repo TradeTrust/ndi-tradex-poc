7a11fa5514b6c57d9798dbcb3c76aa5f810eb21a
cavacado <ngzhaoloong@gmail.com>
2023-11-07 09:50:07 +0800

    feat: add new error case plus doc for invalid idvc | add e2e test for specific document

--- integration/combined-vc.spec.ts
+++ integration/combined-vc.spec.ts
@@ -42,6 +42,19 @@ test("should show correct custom error message when uploading an invalid ttv4-al
     .ok();
 });
 
+test("should show correct custom error message when uploading an invalid ttv4-alpha combined VC, when idVc is invalid", async (t) => {
+  await t.setFilesToUpload("input[type=file]", [
+    "../fixtures/v4/tt/did-idvc-wrapped-signed-idvc-invalid.json",
+  ]);
+
+  await t.wait(5000);
+  await t
+    .expect(
+      Selector("p").withText("the idvc in the document is invalid").exists
+    )
+    .ok();
+});
+
 test("should show unexpected error message when uploading an invalid ttv4-alpha combined VC, when combined VC's signature has been tampered", async (t) => {
   await t.setFilesToUpload("input[type=file]", [
     "../fixtures/v4/tt/did-idvc-wrapped-signed-tampered-signature.json",
